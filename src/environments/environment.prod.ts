5dcbc00e753e09b75972331207641545ebe8dfb7
Carlos Mendez <calicci.gardachi@gmail.com>
2018-01-27 15:11:42 -1000

    added firebase config to environment.prod.ts for prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyAII-sV00NAOL7oUMJpurcVKRzXvFSAjf8',
+        authDomain: 'vets2data.firebaseapp.com',
+        databaseURL: 'https://vets2data.firebaseio.com/',
+        projectId: 'vets2data',
+        storageBucket: 'vets2data.appspot.com',
+        messagingSenderId: '279296452878'
+    }
 };
